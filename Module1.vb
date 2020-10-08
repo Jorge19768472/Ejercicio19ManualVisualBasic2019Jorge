5eada8c26e3cbf86ab342ee298e622877a67f85a
jorge <geordi_j_laforge@hotmail.com>
2020-10-08 15:38:32 +0200

    Ampñiado codigp sacado de http://www.tutorialesprogramacionya.com/visualbasicya/detalleconcepto.php?punto=14&codigo=14&inicio=0  para el manual que estoy creando

--- Module1.vb
+++ Module1.vb
@@ -1,12 +1,48 @@
 ﻿Module Module1
+    Public Class TablaMultiplicar
+        'en esta clase crearermos un sub para recoger el valor
+        ' si el valor es -1  sale del proceso
+
+
+        Public Sub CargarValor()
+            ' declara la valor dentro de a clase 
+            Dim valor As Integer
+
+            Do
+                Console.SetCursorPosition(10, 5)
+                Console.Write("Ingres un valor (-1 para salir): ")
+                valor = Console.ReadLine
+                If valor <> -1 Then
+                    ' es distinto a -1
+                    Calcular(valor)
+
+
+
+                End If
+            Loop While valor <> -1
+        End Sub
+        Public Sub Calcular(ByVal v As Integer)
+            ' declaracion de la variable
+            Dim f, Tabla As Integer
+            For f = 1 To 10
+                Tabla = f * v
+                Console.SetCursorPosition(10, 5 + f)
+                Console.WriteLine(v & " * " & f & " = " & Tabla)
+
+            Next
+
+
+        End Sub
+    End Class
 
     Sub Main()
-        ' Plantilla Ejercicio19ManualVisualBasic2019Jorge
-        ' En esta plantilla tendrás lo basico para crear un menu
+        ' Plantilla Ejercicio19ManualVisualBasic2019conl un menu
         ' con un marco que podras ver como se crear mas rapido lento
-        ' segundo aumentes o bajes el valor del for en el metodo Temprizador
+        ' segundo aumentes o bajes el valor del for en el metodo Temprizador' progrmama viene de esta web.
 
+        'http://www.tutorialesprogramacionya.com/visualbasicya/detalleconcepto.php?punto=14&codigo=14&inicio=0
         Ejercicio19ManualVisualBasic2019Jorge()
+
 
     End Sub
     Sub Ejercicio19ManualVisualBasic2019Jorge()
@@ -19,7 +55,7 @@
         Console.SetCursorPosition(5, 5)
         Console.Write("0.- Salida")
         Console.SetCursorPosition(5, 7)
-        Console.Write("1.- Oocion 1")
+        Console.Write("1.- Mutiplicacion por clase")
         Console.SetCursorPosition(5, 9)
         Console.Write("2. -Opcion 2")
         'Añadrir mas opciones segun vuestras necesidades
@@ -86,11 +122,28 @@
     End Sub
     Sub Eleccion(opcion)
         ' este codigo distribulle el resto de opcines de codigo
+        Dim verdad As Boolean
+        Dim tablamultiplicar As New TablaMultiplicar
+
         Select Case opcion
             Case 0
                 Salida()
                 Return
             Case 1
+                Do
+                    Try
+                        ' Si va odo bien
+                        tablamultiplicar.CargarValor()
+                        verdad = True
+
+                    Catch ex As Exception
+                        ' si hay algun error
+                        Console.SetCursorPosition(10, 7)
+                        Console.Write(ex.Message)
+                    End Try
+
+                Loop Until verdad = True
+
                 ' Aqui meter nuevo codigo
             Case 2
                 'Aqui metes nevo codigo
